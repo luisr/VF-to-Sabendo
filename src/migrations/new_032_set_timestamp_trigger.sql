0ec441b3eb5d7ffbeb82f78c832e1654a82859cf
luisr <luisribeirolima@gmail.com>
2025-08-16 11:47:39 -0300

    Add timestamp trigger for core tables

--- src/migrations/new_032_set_timestamp_trigger.sql
+++ src/migrations/new_032_set_timestamp_trigger.sql
@@ -13,17 +13,17 @@ $$;
 -- Attach trigger to projects
 DROP TRIGGER IF EXISTS set_timestamp ON public.projects;
 CREATE TRIGGER set_timestamp
-BEFORE INSERT OR UPDATE ON public.projects
+BEFORE UPDATE ON public.projects
 FOR EACH ROW EXECUTE FUNCTION public.set_timestamp();
 
 -- Attach trigger to tasks
 DROP TRIGGER IF EXISTS set_timestamp ON public.tasks;
 CREATE TRIGGER set_timestamp
-BEFORE INSERT OR UPDATE ON public.tasks
+BEFORE UPDATE ON public.tasks
 FOR EACH ROW EXECUTE FUNCTION public.set_timestamp();
 
 -- Attach trigger to task_observations
 DROP TRIGGER IF EXISTS set_timestamp ON public.task_observations;
 CREATE TRIGGER set_timestamp
-BEFORE INSERT OR UPDATE ON public.task_observations
+BEFORE UPDATE ON public.task_observations
 FOR EACH ROW EXECUTE FUNCTION public.set_timestamp();
